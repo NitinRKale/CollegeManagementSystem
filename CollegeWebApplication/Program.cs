336926f3ee8fb07a71ed0bb9f7bb426042622d31
NitinRKale <azure.nitinkale@gmail.com>
2026-03-22 13:54:05 +0530

    Identity implemented

--- CollegeWebApplication/Program.cs
+++ CollegeWebApplication/Program.cs
@@ -96,7 +96,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}")
+    pattern: "{controller=Student}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 app.Run();
